e54c924306f7df69ef044be99877b27cafbe0aae
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2020-12-10 15:30:20 -0700

    parallel working, plus combining s, p, chrom, and pos into 1 dataframe

--- s/s_syncfile.R
+++ s/s_syncfile.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 
 suppressMessages(library(poolSeq))
+suppressMessages(library(parallel))
 # options(error = recover)
 
 get_info <- function(infopath) {
@@ -24,9 +25,9 @@ update_info <- function(info, sync) {
 
 estimateSH_one_locus = function(sync, Ne, info, chrom, pos) {
     traj = af.traj(sync, chrom, pos, repl=info$repl)
-    print(traj)
+    # print(traj)
     est_p <- estimateSH(traj, Ne=Ne, t=info$gen_levels, h=0.5, simulate.p.value=TRUE)
-    print(est_p)
+    # print(est_p)
     return(est_p)
 }
 
@@ -34,8 +35,19 @@ estimateSH_one_locus = function(sync, Ne, info, chrom, pos) {
 estimateSH_individual_loci <- function(sync, Ne, info) {
     out = sync
     chrom_pos = cbind(info$chrom, info$pos)
-    s_vals = apply(chrom_pos, 1, function(x) {estimateSH_one_locus(sync, Ne, info, x[1], x[2])})
-    return(s_vals)
+    chrom_pos_list = split(chrom_pos, seq(nrow(chrom_pos)))
+    s_vals = mclapply(chrom_pos_list, function(x) {estimateSH_one_locus(sync, Ne, info, x[1], x[2])})
+    return(list(chrom_pos, s_vals))
+}
+
+gets_from_ests <- function(ests) {
+    s_vals = mclapply(ests, function(x) {x$s})
+    return(unlist(s_vals))
+}
+
+getp_from_ests <- function(ests) {
+    p_vals = mclapply(ests, function(x) {x$p.value})
+    return(unlist(p_vals))
 }
 
 main = function() {
@@ -46,13 +58,13 @@ main = function() {
     info = get_info(infopath)
     mySync <- read.sync(file=syncpath, gen=info$gen, repl=info$repl)
     info = update_info(info, mySync)
-    print("mysync")
-    print(str(mySync))
-    print("info")
-    print(str(info))
+    # print("mysync")
+    # print(str(mySync))
+    # print("info")
+    # print(str(info))
     myTraj = af.traj(mySync, info$chrom, info$pos, info$repl)
-    print("mytraj")
-    print(myTraj)
+    # print("mytraj")
+    # print(myTraj)
     est_nes = rep(NA, length(info$repl_levels))
     for (repl in info$repl_levels) {
         myTraj_repltemp = af.traj(mySync, info$chrom, info$pos, repl)
@@ -80,17 +92,26 @@ main = function() {
     mean_ne = mean(est_nes)
     # est_ne = estimateNe(p0=mytraj1[,"F0"], pt=mytraj1[,"F10"], cov0=mycov1[,"F0.R1.cov"], covt=mycov1[,"F10.R1.cov"], t=10, Ncensus=1000, poolSize=c(300, 300))
     
-    print("est_nes")
-    print(est_nes)
-    print(str(est_nes))
+    # print("est_nes")
+    # print(est_nes)
+    # print(str(est_nes))
     
-    est_p <- estimateSH(myTraj, Ne=mean_ne, t=info$gen_levels, h=0.5, simulate.p.value=TRUE)
-    print("est_p")
-    print(est_p)
-    print(str(est_p))
+    # est_p <- estimateSH(myTraj, Ne=mean_ne, t=info$gen_levels, h=0.5, simulate.p.value=TRUE)
+    # print("est_p")
+    # print(est_p)
+    # print(str(est_p))
     # print(confint(est))
-    est_all_p <- estimateSH_individual_loci(mySync, mean_ne, info)
-    print(est_all_p)
+    est_list <- estimateSH_individual_loci(mySync, mean_ne, info)
+    chrom_pos = est_list[[1]]
+    est_all_p = est_list[[2]]
+    # print(chrom_pos)
+    # print(est_all_p)
+    s_vals = gets_from_ests(est_all_p)
+    p_vals = getp_from_ests(est_all_p)
+    # print(p_vals)
+    # print(s_vals)
+    out = cbind(chrom_pos, s_vals, p_vals)
+    print(out)
 }
 
 main()
--- s/test_s_syncfile.sh
+++ s/test_s_syncfile.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+export MC_CORES=8
+
 exec ./s_syncfile.R \
     ex8.sync \
     ex8.info
