2de6b8c9efeac2e42481906888876c4c1f986b60
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2021-02-22 18:05:38 -0700

    go build and install

--- s/install.sh
+++ s/install.sh
@@ -5,3 +5,5 @@ cp s_syncfile.R ~/mybin/s_syncfile
 cp get_partial_s.R ~/mybin/get_partial_s
 cp manhat_plot_s_and_p.py ~/mybin/manhat_plot_s_and_p
 cp tukey_s.py ~/mybin/tukey_s
+cp filter_goods ~/mybin/filter_goods
+cp combine_goods ~/mybin/combine_goods
