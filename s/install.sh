b82b7d9113e7fa13380923ad95f9a9a1dba1a1dd
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2021-03-04 17:03:19 -0700

    install get_partial_s_repl

--- s/install.sh
+++ s/install.sh
@@ -3,6 +3,7 @@ set -e
 
 cp s_syncfile.R ~/mybin/s_syncfile
 cp get_partial_s.R ~/mybin/get_partial_s
+cp get_partial_s_repl.R ~/mybin/get_partial_s_repl
 cp manhat_plot_s_and_p.py ~/mybin/manhat_plot_s_and_p
 cp tukey_s.py ~/mybin/tukey_s
 cp filter_goods ~/mybin/filter_goods
