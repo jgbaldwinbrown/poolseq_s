471994f863298794e943824baaed7ff9832baf10
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2021-03-29 14:53:55 -0600

    added win to install

--- s/install.sh
+++ s/install.sh
@@ -2,6 +2,7 @@
 set -e
 
 cp s_syncfile.R ~/mybin/s_syncfile
+cp s_syncfile_win.R ~/mybin/s_syncfile_win
 cp get_partial_s.R ~/mybin/get_partial_s
 cp get_partial_s_repl.R ~/mybin/get_partial_s_repl
 cp manhat_plot_s_and_p.py ~/mybin/manhat_plot_s_and_p
