b76745c737775e59b4cb497df6060cdc5b749867
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2021-05-20 10:34:42 -0600

    added types for manhat

--- s/manhat_plot_s_and_p.py
+++ s/manhat_plot_s_and_p.py
@@ -5,8 +5,9 @@ import manhatify as mh
 import pandas as pd
 import math
 import argparse
+from typing import Optional, Union, List, Dict, Tuple
 
-def plot_s(genedata, chrom_offsets, outpath, ymin, ymax):
+def plot_s(genedata: pd.DataFrame, chrom_offsets: pd.DataFrame, outpath: str, ymin: Optional[float], ymax: Optional[float]) -> None:
     if (not ymin) or (not ymax):
         ymin = max(min(genedata["s"]), -1)
         ymax = min(max(genedata["s"]), 1)
@@ -25,7 +26,7 @@ def plot_s(genedata, chrom_offsets, outpath, ymin, ymax):
         ylim = (ymin,ymax)
     )
 
-def plot_p(genedata, chrom_offsets, outpath, ymin, ymax):
+def plot_p(genedata: pd.DataFrame, chrom_offsets: pd.DataFrame, outpath: str, ymin: Optional[float], ymax: Optional[float]) -> None:
     if (not ymin) or (not ymax):
         ymin = max(min(genedata["p"]), 0)
         ymax = max(genedata["p"])
@@ -44,8 +45,8 @@ def plot_p(genedata, chrom_offsets, outpath, ymin, ymax):
         ylim = (ymin,ymax)
     )
 
-def get_args():
-    parser = argparse.ArgumentParser("Generate Manhattan plots of S and P based on the output of poolseq_s.")
+def get_args() -> Tuple[str, str, str, Optional[float], Optional[float], Optional[float], Optional[float]]:
+    parser: argparse.ArgumentParser = argparse.ArgumentParser("Generate Manhattan plots of S and P based on the output of poolseq_s.")
     parser.add_argument("data", help="Data to plot (required).")
     parser.add_argument("chrlens", help="Bed files specifying chromosome lengths (required).")
     parser.add_argument("-o", "--out_prefix", help="Bed files specifying chromosome length (default=\"out\").", default = "out")
@@ -54,17 +55,25 @@ def get_args():
     parser.add_argument("-p", "--pmin", help="Y minimum for plotting p value (default = minimum of data).", type=float, default=None)
     parser.add_argument("-P", "--pmax", help="Y maximum for plotting p value (default = minimum of data).", type=float, default=None)
 
-    args = parser.parse_args()
+    args: argparse.Namespace = parser.parse_args()
 
     return (args.data, args.chrlens, args.out_prefix, args.ymin, args.ymax, args.pmin, args.pmax)
 
-def main():
+def main() -> None:
+    data_inpath: str
+    chrlen_bed_inpath: str
+    out_prefix: str
+    ymin: Optional[float]
+    ymax: Optional[float]
+    pmin: Optional[float]
+    pmax: Optional[float]
     data_inpath, chrlen_bed_inpath, out_prefix, ymin, ymax, pmin, pmax = get_args()
     # data_inpath = sys.argv[1]
     # chrlen_bed_inpath = sys.argv[2]
     # out_prefix = sys.argv[3]
     with open(chrlen_bed_inpath, "r") as inconn:
         chrlens = mh.get_chrom_lens_from_bed(inconn)
+    gdata: pd.DataFrame
     gdata = mh.get_data_from_table(data_inpath, column_names = ["Scaffold", "Position", "s", "p"], header_row = 0)
     gdata["s"] = gdata["s"].apply(pd.to_numeric, errors = "coerce")
     gdata["p"] = gdata["p"].apply(pd.to_numeric, errors = "coerce")
@@ -73,6 +82,8 @@ def main():
     # except:
     #     pass
     # print(gdata)
+    genedata: pd.DataFrame
+    chrom_offsets: pd.DataFrame
     genedata, chrom_offsets = mh.manhatify(gdata, chrlens, chrom_col = "Scaffold", bp_col = "Position", val_col = "s", offset = 10000)
     # print(genedata)
     # print(chrom_offsets)
