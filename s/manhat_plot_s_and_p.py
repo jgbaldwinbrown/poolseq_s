9af62c24e5a0a8e1e19d1f034a64486f4191d249
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2021-05-20 10:21:33 -0600

    manht_plot using args

--- s/manhat_plot_s_and_p.py
+++ s/manhat_plot_s_and_p.py
@@ -4,10 +4,12 @@ import sys
 import manhatify as mh
 import pandas as pd
 import math
+import argparse
 
-def plot_s(genedata, chrom_offsets, outpath):
-    ymin = max(min(genedata["s"]), -1)
-    ymax = min(max(genedata["s"]), 1)
+def plot_s(genedata, chrom_offsets, outpath, ymin, ymax):
+    if (not ymin) or (not ymax):
+        ymin = max(min(genedata["s"]), -1)
+        ymax = min(max(genedata["s"]), 1)
     mh.plot_manhat(genedata,
         outpath,
         chrom_offsets,
@@ -23,9 +25,10 @@ def plot_s(genedata, chrom_offsets, outpath):
         ylim = (ymin,ymax)
     )
 
-def plot_p(genedata, chrom_offsets, outpath):
-    ymin = max(min(genedata["p"]), 0)
-    ymax = max(genedata["p"])
+def plot_p(genedata, chrom_offsets, outpath, ymin, ymax):
+    if (not ymin) or (not ymax):
+        ymin = max(min(genedata["p"]), 0)
+        ymax = max(genedata["p"])
     mh.plot_manhat(genedata,
         outpath,
         chrom_offsets,
@@ -41,10 +44,25 @@ def plot_p(genedata, chrom_offsets, outpath):
         ylim = (ymin,ymax)
     )
 
+def get_args():
+    parser = argparse.ArgumentParser("Generate Manhattan plots of S and P based on the output of poolseq_s.")
+    parser.add_argument("data", help="Data to plot (required).")
+    parser.add_argument("chrlens", help="Bed files specifying chromosome lengths (required).")
+    parser.add_argument("-o", "--out_prefix", help="Bed files specifying chromosome length (default=\"out\").", default = "out")
+    parser.add_argument("-y", "--ymin", help="Y minimum for plotting selection coefficient (default = minimum of data).", type=float, default=None)
+    parser.add_argument("-Y", "--ymax", help="Y maximum for plotting selection coefficient (default = minimum of data).", type=float, default=None)
+    parser.add_argument("-p", "--pmin", help="Y minimum for plotting p value (default = minimum of data).", type=float, default=None)
+    parser.add_argument("-P", "--pmax", help="Y maximum for plotting p value (default = minimum of data).", type=float, default=None)
+
+    args = parser.parse_args()
+
+    return (args.data, args.chrlens, args.out_prefix, args.ymin, args.ymax, args.pmin, args.pmax)
+
 def main():
-    data_inpath = sys.argv[1]
-    chrlen_bed_inpath = sys.argv[2]
-    out_prefix = sys.argv[3]
+    data_inpath, chrlen_bed_inpath, out_prefix, ymin, ymax, pmin, pmax = get_args()
+    # data_inpath = sys.argv[1]
+    # chrlen_bed_inpath = sys.argv[2]
+    # out_prefix = sys.argv[3]
     with open(chrlen_bed_inpath, "r") as inconn:
         chrlens = mh.get_chrom_lens_from_bed(inconn)
     gdata = mh.get_data_from_table(data_inpath, column_names = ["Scaffold", "Position", "s", "p"], header_row = 0)
@@ -59,11 +77,11 @@ def main():
     # print(genedata)
     # print(chrom_offsets)
     try:
-        plot_s(genedata, chrom_offsets, out_prefix + "_s.pdf")
+        plot_s(genedata, chrom_offsets, out_prefix + "_s.pdf", ymin, ymax)
     except:
         pass
     try:
-        plot_p(genedata, chrom_offsets, out_prefix + "_p.pdf")
+        plot_p(genedata, chrom_offsets, out_prefix + "_p.pdf", pmin, pmax)
     except:
         pass
 
