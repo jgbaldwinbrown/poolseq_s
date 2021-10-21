4522205d7f74df07abb9fd9b23531cc6f093a9e3
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2021-10-21 16:55:03 -0600

    updated test.sh

--- s/ne_multi/test.sh
+++ s/ne_multi/test.sh
@@ -1,1 +1,1 @@
-go run ne_multi.go -d black_tle30_names_bitted_repall.split_goods_f0.1_c10.sync.gz -i black_pooled_info_bitted.txt -n 3 -o test -p .01 -r 0
+go run ne_multi.go -d black_tle30_names_bitted_repall.split_goods_f0.1_c10.sync.gz -i black_pooled_info_bitted.txt -n 3 -o test -p .01 -r 0 -j 2
